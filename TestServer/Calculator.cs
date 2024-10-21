dbaef528308f6e7712932458cebc62adbe2d53f0
Chuck-man <sgold2398@gmail.com>
2024-10-21 14:30:18 +0400

    Changes

--- TestServer/Calculator.cs
+++ TestServer/Calculator.cs
@@ -1,7 +1,7 @@
 ﻿using TestServer.Model;
 
 namespace TestServer;
-
+//Calculator class
 public static class Calculator
 {
     public static int Sum(IEnumerable<Number> numbers) => numbers.Sum(n => n.Value);
